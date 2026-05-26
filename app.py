586dbb97fface32fae4e27660fae8782c1d39e25
Raj Kumar <Analyst.k.raj@gmail.com>
2026-05-26 22:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,26 @@ import streamlink
 st.set_page_config(layout = "wide")
 st.title("Live Youtube Multi stream Dashboard")
 
+youtube_urls =[
+  "https://www.youtube.com/watch?v=j2knrqAzYVY",
+  "https://www.youtube.com/watch?v=LPZh9BOjkQs",
+  "https://www.youtube.com/watch?v=X8F9JfCUWrs",
+  "https://www.youtube.com/watch?v=D1eL1EnxXXQ"
+]
+
+# Define a new function
+def get_stream_url(youtube_urls):
+  try
+  # return the array of video quality of streams
+    streams = streamlink.streams(youtube_urls)
+
+    # check for best video quality
+    if 'best' in streams:
+      return streams['best'].url
+    return None
+  except Exception as e:
+    st.error(f"Error loading stram: {e}")
+    return None
+      
+  
+  
